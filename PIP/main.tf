a075b6de6f57a1778813c4e2bd10019d475902d2
vijay <vijay@example.com>
2025-07-09 23:39:17 +0530

    Public IP child folder

---
+++ PIP/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_public_ip" "PIP" {
+  name                = var.public_ip_name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  allocation_method   = "Static"
+}
---
+++ PIP/variables.tf
@@ -0,0 +1,12 @@
+variable "public_ip_name" {
+  description = "The name of the public IP address"
+  type        = string
+}
+variable "resource_group_name" {    
+  description = "The name of the resource group"
+  type        = string
+}
+variable "resource_group_location" {
+  description = "The location of the resource group"
+  type        = string
+}
